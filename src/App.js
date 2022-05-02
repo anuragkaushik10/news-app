482de0aa8dbd423108ca03a8071c9669ef7d8b50
anuragkaushik10 <ak10anurag@gmail.com>
2022-05-03 01:15:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import "./App.css";
 export default function App() {
   const [data, setData] = useState([]);
 
-  function callApi() {
+  async function callApi() {
     const key = "4e04063722cb42fd90c9659bcc0be9ac";
     let url =
       "https://newsapi.org/v2/everything?q=tesla&from=2022-04-02&sortBy=publishedAt&apiKey=4e04063722cb42fd90c9659bcc0be9ac";
-    axios.get(url).then((response) => setData(response));
+    await axios
+      .get(url)
+      .then((response) => setData(response))
+      .catch((error) => console.log("Error ", error));
   }
   useEffect(() => {
     callApi();
   }, []);
-  // console.log(data);
+  console.log(data);
   return (
     <>
       <NewsProvider.Provider value={data}>
