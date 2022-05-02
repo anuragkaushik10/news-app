18a72f3461ae1331b1f115b15f09efd4a4bc6cda
anuragkaushik10 <ak10anurag@gmail.com>
2022-05-03 01:02:11 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
   const [data, setData] = useState([]);
 
   async function callApi() {
+    const key = "4e04063722cb42fd90c9659bcc0be9ac";
     let url =
       "https://newsapi.org/v2/everything?q=tesla&from=2022-04-02&sortBy=publishedAt&apiKey=4e04063722cb42fd90c9659bcc0be9ac";
     await axios.get(url).then((response) => setData(response));
--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -1,18 +1,20 @@
 import React from "react";
 
 export default function NewsArticle({ data }) {
+  console.log(data.url);
   return (
     <>
-      <div className="news">
-        <h1 className="news-title">{data.title}</h1>
-        <p className="news-description">{data.description}</p>
-        <span className="news-author">{data.author}</span>
-        <br />
-        <span className="news-published news-info">{data.publishedAt}</span>
-        <br />
-
-        <span className="news-source news-info">{data.source.name}</span>
-      </div>
+      <a href={data.url} target="_blank">
+        <div className="news">
+          <h1 className="news-title">{data.title}</h1>
+          <p className="news-description">{data.description}</p>
+          <span className="news-author">{data.author}</span>
+          <br />
+          <span className="news-published news-info">{data.publishedAt}</span>
+          <br />
+          <span className="news-source news-info">{data.source.name}</span>
+        </div>
+      </a>
     </>
   );
 }
