4fae0fe7a1637589e3be38ce90bf41c76ecdb308
anuragkaushik10 <ak10anurag@gmail.com>
2022-05-03 23:12:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ export default function App() {
   const [data, setData] = useState([]);
 
   async function callApi() {
-    const key = "4e04063722cb42fd90c9659bcc0be9ac";
+    const key = "2aefb6f341766ede9a92abb5fd20fa81";
     let url =
-      "https://newsapi.org/v2/everything?q=tesla&from=2022-04-02&sortBy=publishedAt&apiKey=4e04063722cb42fd90c9659bcc0be9ac";
+      "https://gnews.io/api/v4/search?q=example&token=2aefb6f341766ede9a92abb5fd20fa81&lang=en";
     await axios
       .get(url)
       .then((response) => setData(response))
